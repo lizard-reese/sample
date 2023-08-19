cdb8b04cafa3c6e0c19a7c3290ef537004986f48
lizard-reese <142461932+lizard-reese@users.noreply.github.com>
2023-08-18 23:11:23 -0400

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,30 +1,58 @@
 import numpy as np
 import numpy.random as rand
 
+import time
+import timeit
+
 import matplotlib.pyplot as plt
 
-n = 50
+N = 128
+M = 16
 
-sample = np.arange(0, n, 1)
-rand.shuffle(sample)
+def BubbleSort(n, printSorts = False):
+	A = np.arange(n)
+	rand.shuffle(A)
 
-print(sample)
+	st = time.time()
 
-def BubbleSort(sample, printSorts = False):
+	while not np.all(A[:-1] <= A[1:]):
 
-	while not np.all(sample[:-1] <= sample[1:]):
+		for i in range(len(A) - 1):
 
-		for i in range(len(sample) - 1):
-
-			if sample[i] > sample[i+1]:
-				sample[i], sample[i+1] = sample[i+1], sample[i]
+			if A[i] > A[i+1]:
+				A[i], A[i+1] = A[i+1], A[i]
 
 		if printSorts:
-			print(sample)
+			print(A)
 
-	return(sample)
+	et = time.time()
+	rt = et-st
+	
+	return(rt*1000)
 
-BubbleSort(sample, printSorts = True)
+
+fig, ax = plt.subplots()
+
+domain = np.arange(N)
+avg = []
+
+plt.ylabel('Runtime (ms)')
+plt.xlabel('Array Size')
+
+for n in domain:
+	data = []
+	
+	for m in range (M):
+		data.append(BubbleSort(n))
+		
+	avg.append(np.average(data))
+
+regression = np.poly1d(np.polyfit(domain, avg, 2))
+
+plt.plot(domain, regression(domain), '-', color = (0.1, 0.3, 0.7))
+plt.plot(domain, avg, 'b.', color = (0.1, 0.3, 0.7))
+
+plt.show()
 
 
 
